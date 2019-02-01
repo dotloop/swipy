33a0f4a159e70a00d7530195c36f9efae2d5babd
Elodie Ferrais <elodie.ferrais@gmail.com>
2019-02-01 13:20:11 -0800

    Add button click feedback

--- app/src/main/java/com/dotloop/sweepy/RecyclerViewAdapter.kt
+++ app/src/main/java/com/dotloop/sweepy/RecyclerViewAdapter.kt
@@ -7,13 +7,20 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class RecyclerViewAdapter(
-    private val dataSet: List<String>) : RecyclerView.Adapter<RecyclerViewAdapter.SimpleViewHolder>() {
+    private val dataSet: List<String>,
+    private val listener: ActionListener) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
     private val handler: SwipableCellHandler = SwipableCellHandler()
 
-    class SimpleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         internal val swipeLayout: SwipeLayout = itemView.findViewById(R.id.swipe_layout)
-        internal val textViewPos: TextView = itemView.findViewById(R.id.text)
+        internal val titleView: TextView = itemView.findViewById(R.id.text)
+        internal val archiveActionView: View = itemView.findViewById(R.id.archive)
+        internal val deleteActionView: View = itemView.findViewById(R.id.delete)
+        internal val phoneActionView: View = itemView.findViewById(R.id.phone)
+        internal val emailActionView: View = itemView.findViewById(R.id.email)
+        internal val txtmessageActionView: View = itemView.findViewById(R.id.txtmessage)
+
 
         init {
             swipeLayout.addDrag(SwipeLayout.DragEdge.LEFT_EDGE, swipeLayout.findViewById(R.id.swipe_left_action))
@@ -21,15 +28,21 @@ class RecyclerViewAdapter(
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SimpleViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return SimpleViewHolder(view)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(
-        simpleViewHolder: SimpleViewHolder, position: Int) {
-        handler.bindSwipableCell(simpleViewHolder.swipeLayout, position)
-        simpleViewHolder.textViewPos.text = dataSet[position]
+        viewHolder: ViewHolder, position: Int) {
+        handler.bindSwipableCell(viewHolder.swipeLayout, position)
+        viewHolder.titleView.text = dataSet[position]
+        viewHolder.archiveActionView.setOnClickListener { listener.onArchiveClicked() }
+        viewHolder.deleteActionView.setOnClickListener { listener.onDeleteClicked() }
+        viewHolder.emailActionView.setOnClickListener { listener.onEmailClicked() }
+        viewHolder.phoneActionView.setOnClickListener { listener.onPhoneClicked() }
+        viewHolder.txtmessageActionView.setOnClickListener { listener.onTxtmessageClicked() }
+
     }
 
     override fun getItemCount(): Int {
